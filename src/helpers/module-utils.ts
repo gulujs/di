83534195eac1a79203e30c712e4c0c6c8c4ac676
jKey Lu <jkeylu@gmail.com>
2021-12-19 12:05:03 +0000

    feat: add utility function isClassicalModule

--- src/helpers/module-utils.ts
+++ src/helpers/module-utils.ts
@@ -1,5 +1,20 @@
 import { DynamicModule, Type } from '../interfaces';
+import {
+  getModuleExportsMetadata,
+  getModuleImportsMetadata,
+  getModuleProvidersMetadata,
+  getModuleProvidersScanPathsMetadata
+} from './metadata-utils';
 
 export function isDynamicModule(module: Type<unknown> | DynamicModule): module is DynamicModule {
   return !!(module as Partial<DynamicModule>).module;
+}
+
+export function isClassicalModule(module: unknown): module is Type<unknown> {
+  return !!(
+    getModuleProvidersMetadata(module as Type<unknown>)
+    || getModuleProvidersScanPathsMetadata(module as Type<unknown>)
+    || getModuleImportsMetadata(module as Type<unknown>)
+    || getModuleExportsMetadata(module as Type<unknown>)
+  );
 }
